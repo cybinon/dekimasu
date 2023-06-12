2d88121170b7e39ae5f189532028721f412ee72d
Cybinon <nikorunikk@gmail.com>
2023-06-12 16:01:16 +0800

    version updated

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const http = require("http")
-const { Server } = require("socket.io")
+const app = express()
+
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
 const port = 3000
-
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
 
 app.use(express.static('public'))
 
@@ -14,50 +15,57 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-const players = {}
+const backEndPlayers = {}
 
-io.on(("connection"), (socket) => {
-  console.log("user connected")
-  players[socket.id] = {
-    x: 100 * Math.random(),
-    y: 100 * Math.random(),
-    color: `hsl(${360 * Math.random()},100%, 50%)`
+const SPEED = 10
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  backEndPlayers[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random(),
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`,
+    sequenceNumber: 0
   }
-  io.emit("updatePlayers", players)
 
-  socket.on("disconnect", (reason) => {
+  io.emit('updatePlayers', backEndPlayers)
+
+  socket.on('disconnect', (reason) => {
     console.log(reason)
-    delete players[socket.id]
-    io.emit("updatePlayers", players)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
   })
 
-  socket.on("keydown", (keyCode) => {
-    switch (keyCode) {
-      case "KeyW":
-        players[socket.id].y -= 30
-        console.log("Key w pressed")
-        break;
-      case "KeyA":
-        players[socket.id].x -= 30
-        console.log("Key a pressed")
-        break;
-      case "KeyS":
-        players[socket.id].y += 30
-        console.log("Key s pressed")
-        break;
-      case "KeyD":
-        players[socket.id].x += 30
-        console.log("Key d pressed")
+  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+    backEndPlayers[socket.id].sequenceNumber = sequenceNumber
+    switch (keycode) {
+      case 'KeyW':
+        backEndPlayers[socket.id].y -= SPEED
+        break
+
+      case 'KeyA':
+        backEndPlayers[socket.id].x -= SPEED
+        break
+
+      case 'KeyS':
+        backEndPlayers[socket.id].y += SPEED
+        break
+
+      case 'KeyD':
+        backEndPlayers[socket.id].x += SPEED
+        break
     }
   })
+
+  console.log(backEndPlayers)
 })
 
 setInterval(() => {
-  io.emit("updatePlayers", players)
-}, 10)
-
-
+  io.emit('updatePlayers', backEndPlayers)
+}, 15)
 
 server.listen(port, () => {
-  console.log("hello")
+  console.log(`Example app listening on port ${port}`)
 })
+
+console.log('server did load')
--- public/js/index.js
+++ public/js/index.js
@@ -1,66 +1,170 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-const scoreEl = document.querySelector('#scoreEl')
 const socket = io()
 
-const devicePiexlRatio = window.devicePixelRatio || 1
+const scoreEl = document.querySelector('#scoreEl')
 
-canvas.width = innerWidth * devicePiexlRatio
-canvas.height = innerHeight * devicePiexlRatio
+const devicePixelRatio = window.devicePixelRatio || 1
+
+canvas.width = innerWidth * devicePixelRatio
+canvas.height = innerHeight * devicePixelRatio
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const players = {}
-const projectiles = []
-const enemies = []
-const particles = []
+const frontEndPlayers = {}
 
-socket.on('updatePlayers', (backendPlayers) => {
-  for (const id in backendPlayers) {
-    const backendPlayer = backendPlayers[id]
-    if (!players[id]) {
-      players[id] = new Player({
-        x: backendPlayer.x,
-        y: backendPlayer.y,
+socket.on('updatePlayers', (backEndPlayers) => {
+  for (const id in backEndPlayers) {
+    const backEndPlayer = backEndPlayers[id]
+
+    if (!frontEndPlayers[id]) {
+      frontEndPlayers[id] = new Player({
+        x: backEndPlayer.x,
+        y: backEndPlayer.y,
         radius: 10,
-        color: backendPlayer.color
+        color: backEndPlayer.color
       })
     } else {
-      gsap.to(players[id], {
-        x: backendPlayer.x,
-        y: backendPlayer.y,
-        duration: 0.015,
-        ease: 'linear'
-      })
+      if (id === socket.id) {
+        // if a player already exists
+        frontEndPlayers[id].x = backEndPlayer.x
+        frontEndPlayers[id].y = backEndPlayer.y
+
+        const lastBackendInputIndex = playerInputs.findIndex((input) => {
+          return backEndPlayer.sequenceNumber === input.sequenceNumber
+        })
+
+        if (lastBackendInputIndex > -1)
+          playerInputs.splice(0, lastBackendInputIndex + 1)
+
+        playerInputs.forEach((input) => {
+          frontEndPlayers[id].x += input.dx
+          frontEndPlayers[id].y += input.dy
+        })
+      } else {
+        // for all other players
+
+        gsap.to(frontEndPlayers[id], {
+          x: backEndPlayer.x,
+          y: backEndPlayer.y,
+          duration: 0.015,
+          ease: 'linear'
+        })
+      }
     }
   }
-  for (const id in players) {
-    if (!backendPlayers[id]) {
-      delete players[id]
+
+  for (const id in frontEndPlayers) {
+    if (!backEndPlayers[id]) {
+      delete frontEndPlayers[id]
     }
   }
-  console.log(players)
 })
 
 let animationId
-let score = 0
 function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
 
-  for (const id in players) {
-    const player = players[id]
-    player.draw()
+  for (const id in frontEndPlayers) {
+    const frontEndPlayer = frontEndPlayers[id]
+    frontEndPlayer.draw()
   }
 }
 
 animate()
 
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+const SPEED = 10
+const playerInputs = []
+let sequenceNumber = 0
+setInterval(() => {
+  if (keys.w.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: 0, dy: -SPEED })
+    frontEndPlayers[socket.id].y -= SPEED
+    socket.emit('keydown', { keycode: 'KeyW', sequenceNumber })
+  }
+
+  if (keys.a.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: -SPEED, dy: 0 })
+    frontEndPlayers[socket.id].x -= SPEED
+    socket.emit('keydown', { keycode: 'KeyA', sequenceNumber })
+  }
+
+  if (keys.s.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: 0, dy: SPEED })
+    frontEndPlayers[socket.id].y += SPEED
+    socket.emit('keydown', { keycode: 'KeyS', sequenceNumber })
+  }
+
+  if (keys.d.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: SPEED, dy: 0 })
+    frontEndPlayers[socket.id].x += SPEED
+    socket.emit('keydown', { keycode: 'KeyD', sequenceNumber })
+  }
+}, 15)
 
 window.addEventListener('keydown', (event) => {
-  if (!players[socket.id]) return
-  socket.emit("keydown", event.code)
+  if (!frontEndPlayers[socket.id]) return
+
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = true
+      break
+
+    case 'KeyA':
+      keys.a.pressed = true
+      break
+
+    case 'KeyS':
+      keys.s.pressed = true
+      break
+
+    case 'KeyD':
+      keys.d.pressed = true
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  if (!frontEndPlayers[socket.id]) return
+
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = false
+      break
+
+    case 'KeyA':
+      keys.a.pressed = false
+      break
+
+    case 'KeyS':
+      keys.s.pressed = false
+      break
+
+    case 'KeyD':
+      keys.d.pressed = false
+      break
+  }
 })
